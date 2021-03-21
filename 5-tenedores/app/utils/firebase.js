6dae80501cfa175eb7f2ef8e2e626a549c83d6d0
Antonio Jesus Heredia Castillo <a.heredia.castillo@gmail.com>
2021-03-21 13:52:56 +0100

    Hasta el menu

---
+++ 5-tenedores/app/utils/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyAMeD_po2Jgdb6VrfE_X-wGI8Sc8by1Nlw",
+    authDomain: "tenedores-curso-fdca9.firebaseapp.com",
+    projectId: "tenedores-curso-fdca9",
+    storageBucket: "tenedores-curso-fdca9.appspot.com",
+    messagingSenderId: "432759968536",
+    appId: "1:432759968536:web:ec6835f0d1bceb85d5046d",
+    measurementId: "G-07QSBYGXND"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
